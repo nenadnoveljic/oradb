7b235592b7b285d8a9d0e286ea3bc6e17cf62c96
nenadnoveljic <nenad.noveljic@gmail.com>
2016-11-22 17:53:20 +0100

    Add files via upload

---
+++ hybrid_hist_newdensity.sql
@@ -0,0 +1,42 @@
+/*
+ * hybrid_hist_newdensity.sql
+ *
+ * The query calculates the new density value for hybrid histograms
+ * For more information see http://nenadnoveljic.com/blog/
+ *
+ * Author: Nenad Noveljic
+ *
+ * Input:
+ *   :table_name
+ *   :column_name
+ *
+ * Output:
+ *   NewDensity
+ *
+*/
+
+with pop_v as
+(
+select 
+    sum(endpoint_repeat_count) sum_pop_ep_rp_count ,
+    count(*) pop_value_count
+    from
+        user_tab_histograms uth
+       ,user_tab_col_statistics ucs
+    where
+		uth.table_name   = ucs.table_name
+		and uth.column_name   = ucs.column_name
+		and uth.table_name    = :table_name
+		and uth.column_name   = :column_name
+		and (uth.endpoint_repeat_count - ucs.sample_size/ucs.num_buckets) > 0
+),
+ucs_v as
+( select num_distinct,sample_size from user_tab_col_statistics
+    where table_name    = :table_name and column_name   = :column_name
+)
+select 
+    ( 1 - sum_pop_ep_rp_count/sample_size)/(num_distinct - pop_value_count )
+    NewDensity 
+    from pop_v,ucs_v
+;
+
